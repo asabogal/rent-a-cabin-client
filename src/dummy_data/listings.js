831811f06cb3cf8faf0b59e8dec25ff4a4828107
asabogal <asabogal.git@gmail.com>
2020-02-02 18:49:21 -0500

    add listings dummy data

---
+++ src/dummy_data/listings.js
@@ -0,0 +1,185 @@
+import main1 from './pictures/cabin1/main.jpg';
+import featured1 from './pictures/cabin1/featured.jpg';
+import main2 from './pictures/cabin2/main.jpg';
+import featured2 from './pictures/cabin2/featured.jpg';
+import main3 from './pictures/cabin3/main.jpg';
+import featured3 from './pictures/cabin3/featured.jpg';
+import main4 from './pictures/cabin4/main.jpg';
+import featured4 from './pictures/cabin4/featured.jpg';
+import main5 from './pictures/cabin5/main.jpg';
+import featured5 from './pictures/cabin5/featured.jpg';
+import main6 from './pictures/cabin6/main.jpg';
+import featured6 from './pictures/cabin6/featured.jpg';
+import main7 from './pictures/cabin7/main.jpg';
+import featured7 from './pictures/cabin7/featured.jpg';
+import main8 from './pictures/cabin8/main.jpg';
+import featured8 from './pictures/cabin8/featured.jpg';
+import main9 from './pictures/cabin9/main.jpg';
+import featured9 from './pictures/cabin9/featured.jpg';
+import main10 from './pictures/cabin10/main.jpg';
+import featured10 from './pictures/cabin10/featured.jpg';
+
+
+export const listings = [
+  {
+    id: 1,
+    title: "Tipi Tree",
+    description: 'The perfect place to become insane. Enjoy complete seclusion and claustrophobia in this tiny yet modern tipi style cabin.',
+    location: "Death Forest, PA",
+    coordinates:"41.793407, -78.568276",
+    bedrooms: 1,
+    bathrooms: 1,
+    sleeps: 2,
+    price: 140,
+    pictures: {
+      main: main1,
+      featured: featured1,
+    },
+    userId: 1,
+  },
+  {
+    id: 2,
+    title: "Wood Lodge",
+    description: 'This semi-rustic wood cabin offers the warmth that only an upstate retreat could. You will also get bored an lonely quickly.',
+    location: "Random Forest, NY",
+    coordinates:"43.600369, -74.738566",
+    bedrooms: 5,
+    bathrooms: 4,
+    sleeps: 14,
+    price: 280,
+    pictures: {
+      main: main2,
+      featured: featured2,
+    },
+    userId: 1,
+  },
+  {
+    id: 3,
+    title: "Floe House",
+    description: 'An enjoyable cabin when the sun is out, but the lake view makes up for it on cold, dark, lonely nights.',
+    location: "Freezing Quebec, CA",
+    coordinates:"50.791453, -78.379958",
+    bedrooms: 4,
+    bathrooms: 3,
+    sleeps: 10,
+    price: 250,
+    pictures: {
+      main: main3,
+      featured: featured3,
+    },
+    userId: 1,
+  },
+  {
+    id: 4,
+    title: "Boring Fella",
+    description: 'Just your typical rich person cabin. This place will make you feel like you never left the city at all.',
+    location: "Not The Hamptons, NY",
+    coordinates:"42.027739, -74.494090",
+    bedrooms: 5,
+    bathrooms: 5,
+    sleeps: 12,
+    price: 290,
+    pictures: {
+      main: main4,
+      featured: featured4,
+    },
+    userId: 1,
+  },
+  {
+    id: 5,
+    title: "Cow Hide Ranch",
+    description: 'Perfect for families and friends, this cozy cabin offers plenty of room to relax with lots of leather seating. Leather seating guys!',
+    location: "Thurman NY",
+    coordinates:"43.517644, -73.880307",
+    bedrooms: 7,
+    bathrooms: 5,
+    sleeps: 20,
+    price: 315,
+    pictures: {
+      main: main5,
+      featured: featured5,
+    },
+    userId: 1,
+  },
+  {
+    id: 6,
+    title: "Garden Ville",
+    description: "Surreal fantasies mix with the cold reality that you're and adult vacationing in thie Gnome home.",
+    location: "Tiny Wildernes, NY",
+    coordinates:"43.517644, -73.880307",
+    bedrooms: 2,
+    bathrooms: 1,
+    sleeps: 4,
+    price: 165,
+    pictures: {
+      main: main6,
+      featured: featured6,
+    },
+    userId: 1,
+  },
+  {
+    id: 7,
+    title: "The Mariposa",
+    description: 'This beautifully designed modern cabin offers elegant modern details that will make you forget why you came to the woods in the first place.',
+    location: "Westworld Set, NH",
+    coordinates:"43.332338, -71.054416",
+    bedrooms: 6,
+    bathrooms: 5,
+    sleeps: 14,
+    price: 390,
+    pictures: {
+      main: main7,
+      featured: featured7,
+    },
+    userId: 1,
+  },
+  {
+    id: 8,
+    title: "Kettle House",
+    description: 'A peaceful retreat with modern comforts and beautiful views of trees, tress, and more trees.',
+    location: "Ottawhat?, CA",
+    coordinates:"46.830791, -76.664619",
+    bedrooms: 3,
+    bathrooms: 2,
+    sleeps: 6,
+    price: 220,
+    pictures: {
+      main: main8,
+      featured: featured8,
+    },
+    userId: 1,
+  },
+  {
+    id: 9,
+    title: "Cozy Palace",
+    description: 'The beautiful lake you will make you ponder why you rented the smallest cabin in the lake.',
+    location: "Big Homes, NY",
+    coordinates:"44.271019, -74.311364",
+    bedrooms: 1,
+    bathrooms: 1,
+    sleeps: 2,
+    price: 130,
+    pictures: {
+      main: main9,
+      featured: featured9,
+    },
+    userId: 1,
+  },
+  {
+    id: 10,
+    title: "Pallette Cottage",
+    description: "Cozy charm and modern amenities. Enjoy zero restaurants and no shops close by. You'll appreaciate the hammock while you're here." ,
+    location: "Big Homes, NY",
+    coordinates:"44.271019, -74.311364",
+    bedrooms: 2,
+    bathrooms: 1,
+    sleeps: 2,
+    price: 120,
+    pictures: {
+      main: main10,
+      featured: featured10,
+    },
+    userId: 1,
+  },
+  
+]
