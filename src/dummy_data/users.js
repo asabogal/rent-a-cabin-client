1d7da752f8eba86d0cfc1302aebf45f69d25f27f
asabogal <asabogal.git@gmail.com>
2020-02-02 20:40:11 -0500

    update listings and add users data

---
+++ src/dummy_data/users.js
@@ -0,0 +1,22 @@
+export const users = [
+  {
+    id: 1,
+    username: 'James Wallace',
+    email: 'j@w.com',
+  },
+  {
+    id: 2,
+    username: 'Laura Jones',
+    email: 'l@j.com',
+  },
+  {
+    id: 3,
+    username: 'Markus Lop',
+    email: 'm@l.com',
+  },
+  {
+    id: 4,
+    username: 'Daniel Ace',
+    email: 'd@a.com',
+  },
+]
